b4ded2e2c3c076d31716ddf287dffb03c7f21361
vinnamkim <vinnam.kim@gmail.com>
2019-10-27 01:10:40 +0900

    Add doublenorm2
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- imagenet/my_models/utils.py
+++ imagenet/my_models/utils.py
@@ -8,6 +8,6 @@ EPS = 1e-5
 def zeronorm(x):
     """x : [B, C, H, W]"""
     """x_mean : [B, 1, 1, 1]"""
-    mean = x.flatten(1).mean(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
-    std = x.flatten(1).std(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
+    mean = x.flatten_(1).mean_(1, keepdim=True).unsqueeze_(-1).unsqueeze_(-1)
+    std = x.flatten_(1).std_(1, keepdim=True).unsqueeze_(-1).unsqueeze_(-1)
     return (x - mean) / (std + EPS)
