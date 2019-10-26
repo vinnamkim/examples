d52895899d4d97482e289000f77889972b630791
vinnamkim <vinnam.kim@gmail.com>
2019-10-27 00:52:51 +0900

    Add doublenorm2
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- imagenet/my_models/utils.py
+++ imagenet/my_models/utils.py
@@ -2,3 +2,12 @@
 def zerocenter(x):
     """x : [B, C, H, W]"""
     return x - x.flatten(1).mean(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
+
+EPS = 1e-5
+
+def zeronorm(x):
+    """x : [B, C, H, W]"""
+    """x_mean : [B, 1, 1, 1]"""
+    mean = x.flatten(1).mean(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
+    std = x.flatten(1).std(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
+    return (x - mean) / (std + EPS)
